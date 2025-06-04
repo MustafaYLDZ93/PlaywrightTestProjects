ad13edd171aa675ab9e79dd4242f828c5349ab36
MustafaYLDZ93 <mustafa.yldz093@gmail.com>
2025-06-04 18:00:03 +0300

    ...

--- CustomCommands/checkoutPageCustomCommand.ts
+++
@@ -1,9 +0,0 @@
-import { Page } from '@playwright/test';
-import {selectors} from '../fixtures-Saucedemo/selectors';
-
-export async function checkoutPage(page: Page): Promise<void> {
-    await page.fill(selectors.checkoutFirstName, "Mustafa");
-    await page.fill(selectors.checkoutLastName, "Yıldız");
-    await page.fill(selectors.checkoutPostalCode, "34490");
-    await page.locator(selectors.checkoutContinueButton).click();
-}
